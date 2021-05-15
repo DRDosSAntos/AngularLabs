ff969796c2e41315cb2e260e744d25ea69ae3bf8
DRDosSAntos <dr.dos.santos@belastingdienst.nl>
2021-05-15 14:46:19 +0200

    Angular labs 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Contact} from './models/contact';
+import {Component} from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+
   constructor() {
     this.name = 'world!';
   }
+
+  contacts: Contact[] = [
+    {firstName: 'Sam', surname: 'Smith', email: 'sam.smith@music.com'},
+    {firstName: 'Frank', surname: 'Muscles', email: 'frank@muscles.com'},
+    {firstName: 'Eddy', surname: 'Valentino', email: 'eddy@valfam.co.uk'}
+  ];
+  newContact = {} as Contact;
+
+  addContact(): void {
+    this.contacts.push(this.newContact);
+  }
 }
+
+
+
+
